309b8c3ca3fddfe934472641eca10c796d1aace1
Etech <excellenceseo@gmail.com>
2018-11-26 10:38:52 +0530

    changeMoadel

---
+++ src/app/content/pages/components/settings/auto-matic-tag/auto-matic-tag.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+import { ApiService } from '../../../../../core/services/api.service';
+import { MatDialog, MatDialogConfig, MatChipInputEvent } from '@angular/material';
+import * as moment from 'moment';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+import { Location } from '@angular/common';
+import { NgForm, FormControl, Validators } from '@angular/forms';
+@Component({
+    selector: 'm-auto-matic-tag',
+    templateUrl: './auto-matic-tag.component.html',
+    styleUrls: ['./auto-matic-tag.component.scss']
+})
+export class AutoMaticTagComponent implements OnInit {
+    tag: any;
+    tempList: any;
+    originalcolor = '';
+    originaltitle = '';
+    temp_id: any;
+    // availableColors = color_list;
+    tags = [];
+    // separatorKeysCodes = [ENTER, COMMA];
+    visible = true;
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    tagStatus: any;
+    constructor(private tagupdate: ApiService, private _location: Location) { }
+
+    ngOnInit() {
+        this.tempList = JSON.parse(localStorage.getItem('tempList'));
+        this.tag = JSON.parse(localStorage.getItem('tag'));
+        this.tagStatus = this.tag.active_status;
+        if (this.tag.active_status) {
+            if (this.tag.keyword === null || this.tag.keyword === ['']) {
+                this.tags = [];
+            } else {
+                this.tags = this.tag.keyword.split(',');
+            }
+            this.originaltitle = this.tag.title;
+            this.originalcolor = this.tag.color
+            this.temp_id = this.tag.template_id;
+            if (this.tag['from']) {
+                this.tag['from'] = moment(this.tag['from']).format('YYYY-MM-DD');
+            }
+            if (this.tag['to']) {
+                this.tag['to'] = moment(this.tag['to']).format('YYYY-MM-DD');
+            }
+        }
+    }
+    add(event: MatChipInputEvent): void {
+        const input = event.input;
+        const value = event.value;
+        if ((value || '').trim()) {
+            this.tags.push(value.trim());
+        }
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    remove(tag: any): void {
+        const index = this.tags.indexOf(tag);
+        if (index >= 0) {
+            this.tags.splice(index, 1);
+        }
+        console.log(index);
+
+    }
+
+    save() {
+        if (this.tags.length === 0) {
+            this.tag.keyword = null;
+        } else {
+            this.tag.keyword = this.tags.toString();
+        }
+        this.tag.title = this.originaltitle;
+        this.originalcolor = this.tag.color
+        this.tag.template_id = this.temp_id;
+        this.tagupdate.updateTag(this.tag, this.tag.type).subscribe((data) => {
+            this.close();
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    close() {
+        this._location.back();
+    }
+    enableJobtag(tagStatus) {
+        if (tagStatus) {
+            const body = { id: this.tag.id, status: tagStatus };
+            this.tagupdate.closeJobProfile(body).subscribe(data => {
+                this.close()
+            }, err => {
+                console.log(err);
+            });
+        }
+    }
+
+
+}
--- src/app/content/pages/components/settings/tag-setting/modal/manual-tag/manual-tag.component.ts
+++ src/app/content/pages/components/settings/tag-setting/modal/manual-tag/manual-tag.component.ts
@@ -18,7 +18,8 @@ export class ManualTagComponent implements OnInit {
   }
   save() {
     this.tag.title = this.originalTitle;
-    this.tag.color = this.originalColor;
+    this.originalColor=this.tag.color
+    console.log( this.originalColor);
     this.tagupdate.updateTag(this.tag, this.tag.type).subscribe((data) => {
         this.dialogRef.close('saved');
     }, (err) => {
